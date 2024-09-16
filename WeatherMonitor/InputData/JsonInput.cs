9af051b326df2ce72e56e5ebbeffab870ab79f18
flesten-ali <fbada858@gmail.com>
2024-09-16 18:21:32 +0300

    upload files

---
+++ WeatherMonitor/InputData/JsonInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using WeatherMonitor.PrintConfig;
+using WeatherMonitor.WeatherManagement;
+namespace WeatherMonitor.InputData;
+
+public class JsonInput : IInputData
+{
+    private readonly IPrint _printer;
+
+    public JsonInput(IPrint print)
+    {
+        _printer = print;
+
+    }
+
+    public WeatherData? ParseData(string input)
+    {
+        WeatherData? weatherData = null;
+        try
+        {
+            weatherData = JsonSerializer.Deserialize<WeatherData>(input);
+        }
+        catch
+        {
+            _printer.Log("Invalid Json Format");
+        }
+        return weatherData;
+    }
+
+    public bool IsMatch(string input)
+    {
+        return input.StartsWith('{') && input.EndsWith('}');
+    }
+}
